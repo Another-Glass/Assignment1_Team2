7dbed19895c28844d3461b720ad871473c258080
hyunghilkim <gusrlf14@gmail.com>
2021-11-02 23:28:32 +0900

    두번째 피드백 결과물

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 // Routes
 const posts = require("./routes/post");
 const auth = require("./routes/auth");
+const commment = require("./routes/comment");
 
 const app = express();
+
+const MONGO_URI =
+  "mongodb+srv://khh0604:subi7135@cluster0.3zojk.mongodb.net/news?retryWrites=true&w=majority";
 
 //express 빌트인 body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connecting Success!!"))
+  .catch((e) => console.log(e));
+
 //routes
 app.use("/posts", posts);
 app.use("/auth", auth);
+app.use("/commment", commment);
 
 module.exports = app;
--- routes/comment.js
+++ routes/comment.js
@@ -1,27 +1,20 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const page_ctrl = require("../util/paginate.ctrl");
 const router = express.Router();
 
 //model
-const Post = require("../models/Board");
+const Comment = require("../models/Comment");
 
-// @routes     POST /posts/write
-// @desc       글 작성
+// @routes     POST /comment/write
+// @desc       댓글 작성
 // @access     user
 router.post("/write", auth, (req, res) => {
-  const { title, content, nickname } = req.body;
-  // 간단한 유효성 검사
-  if (!title || !content || !nickname) {
-    return res.status(400).json({ message: "모든 필드를 채워주세요" });
-  }
-  Post.create({ title, content, nickname })
-    .then(({ title, content, nickname, id }) => {
+  const { content } = req.body;
+
+  Comment.create({ content })
+    .then(({ content }) => {
       res.status(200).json({
-        title,
         content,
-        nickname,
-        id,
       });
     })
     .catch((e) => {
@@ -32,17 +25,14 @@ router.post("/write", auth, (req, res) => {
     });
 });
 
-// @routes     GET /posts/detail/:id
-// @desc       글 확인
+// @routes     GET /comment/detail/:id
+// @desc       댓글 확인
 // @access     public
 router.get("/detail/:id", (req, res) => {
   Post.findByPk(req.params.id)
-    .then(({ title, content, nickname, id }) => {
+    .then(({ content }) => {
       res.status(200).json({
-        title,
         content,
-        nickname,
-        id,
       });
     })
     .catch((e) => {
@@ -53,8 +43,8 @@ router.get("/detail/:id", (req, res) => {
     });
 });
 
-// @routes     GET /posts/delete/:id
-// @desc       글 삭제
+// @routes     DELETE /comment/write
+// @desc       댓글 삭제
 // @access     user
 router.delete("/delete/:id", auth, (req, res) => {
   Post.destroy({ where: { id: req.params.id } })
@@ -69,15 +59,12 @@ router.delete("/delete/:id", auth, (req, res) => {
     });
 });
 
-// @routes     GET /posts/edit/:id
-// @desc       글 수정
+// @routes     GET /comment/edit/:id
+// @desc       댓글 수정
 // @access     user
 router.put("/edit/:id", auth, (req, res) => {
-  const { title, content, nickname } = req.body;
-  Post.updateMany(
-    { title, content, nickname },
-    { where: { id: req.params.id } }
-  )
+  const { content } = req.body;
+  Post.updateOne({ content }, { where: { id: req.params.id } })
     .then(() => {
       res.status(200).send("게시물 수정 완료");
     })
